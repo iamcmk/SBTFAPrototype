a39efbbee91d9651d2dccf1db75195c7bba439ab
Conor McKenna <conorpmckenna@gmail.com>
2019-02-26 13:45:26 +0000

    notification test

---
+++ logic.js
@@ -0,0 +1,18 @@
+// - On sub - check the code
+// - If code is correct then pass
+// - If code is not correct then no pass (do what?
+
+
+
+//
+// function checkCode(userCode, setCode) {
+//   if (userCode == setCode) {
+//     // Do something
+//   } else {
+//     // Do something else
+//   };
+// }
+
+Notification.requestPermission(function(status) {
+    console.log('Notification permission status:', status);
+});
